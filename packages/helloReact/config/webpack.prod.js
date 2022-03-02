0de20fd7d457f3b6d1d2598c3f75ca9137bac7f2
rajdatta <rajdatta.bhosale@gmail.com>
2022-03-02 12:42:45 +0000

    some

--- packages/helloReact/config/webpack.prod.js
+++ packages/helloReact/config/webpack.prod.js
@@ -9,7 +9,7 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
-    publicPath: "/helloReact/",
+    publicPath: "/website/helloReact/",
   },
   plugins: [
     new ModuleFederationPlugin({
